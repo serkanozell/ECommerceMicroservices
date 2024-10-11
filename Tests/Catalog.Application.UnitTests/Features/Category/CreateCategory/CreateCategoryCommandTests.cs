2d4ce7902b157ff678984b576f7a2b62a2c5918d
serkanozel399 <serkanozel399@gmail.com>
2024-10-12 00:29:06 +0300

    CatalogService - Unit tests for CreateCategory feature added, NSubstitute added

--- Tests/Catalog.Application.UnitTests/Features/Category/CreateCategory/CreateCategoryCommandTests.cs
+++ Tests/Catalog.Application.UnitTests/Features/Category/CreateCategory/CreateCategoryCommandTests.cs
@@ -1,28 +1,75 @@
-﻿using Catalog.Application.Dtos.Category;
-using Catalog.Application.Features.Category.CreateCategory;
-using MediatR;
+﻿using Catalog.Application.Features.Category.CreateCategory;
+using Catalog.Application.Repositories;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using NSubstitute;
 
 namespace Catalog.Application.UnitTests.Features.Category.CreateCategory
 {
-    //public class CreateCategoryCommandTests() : IClassFixture<CreateCategoryCommand>
-    //{      
-    //    public CreateCategoryCommandTests(ISender sender, CreateCategoryDto createCategoryDto)
-    //    {
-    //        sender = IClassFixture
-    //    }
-    //    private static readonly CreateCategoryDto createCategoryDto = new CreateCategoryDto(Name: "Test1");
+    public class CreateCategoryCommandTests
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly CreateCategoryValidator _validator;
+        public CreateCategoryCommandTests()
+        {
+            _unitOfWork = Substitute.For<IUnitOfWork>();
+            _validator = new CreateCategoryValidator();
+        }
+        [Fact]
+        public async Task WhenCategoryNameIsNotNull_Should_Added()
+        {
+            // Arrange
+            var command = new CreateCategoryCommand(new Dtos.Category.CreateCategoryDto(Name: "name"));
+            var handler = new CreateCategoryCommandHandler(_unitOfWork);
 
-    //    [Fact]
-    //    public async Task WhenCategoryNameIsNotNull_Should_Added()
-    //    {
-    //        // Arrange
-    //        var command = new CreateCategoryCommand(createCategoryDto);
+            // Act
+            var result = await handler.Handle(command, CancellationToken.None);
 
-    //        // Act
-    //        var result = await sender.Send(command);
 
-    //        // Assert
-    //        Assert.True(result.IsSuccess);
-    //    }
-    //}
+            // Assert
+            result.Id.Should().NotBeEmpty();
+            result.Should().BeOfType<CreateCategoryResult>();
+        }
+
+        [Fact]
+        public async Task WhenCategoryNameIsNull_Should_Return_Error()
+        {
+            // Arrange
+            var command = new CreateCategoryCommand(new Dtos.Category.CreateCategoryDto(Name: ""));
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldHaveAnyValidationError();
+            result.ShouldHaveValidationErrorFor(c => c.CreateCategoryDto.Name);
+        }
+
+        [Fact]
+        public async Task WhenCategoryNameIsNotNull_ShouldNot_Return_Error()
+        {
+            // Arrange
+            var command = new CreateCategoryCommand(new Dtos.Category.CreateCategoryDto(Name: "test"));
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public async Task WhenCategoryNameIsBiggerThanMaxLimit_Should_Return_Error()
+        {
+            // Arrange
+            var command = new CreateCategoryCommand(new Dtos.Category.CreateCategoryDto(Name: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"));
+
+            // Act
+            var result = await _validator.TestValidateAsync(command);
+
+            // Assert
+            result.ShouldHaveAnyValidationError();
+            result.ShouldHaveValidationErrorFor(c => c.CreateCategoryDto.Name);
+        }
+    }
 }
