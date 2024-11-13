722fa3d5c5c6f06571cffef2ae58c8da92c5e7d7
serkanozel399 <serkanozel399@gmail.com>
2024-11-13 21:59:52 +0300

    DeleteCategoryCommandTest class created

---
+++ Tests/Catalog.Application.UnitTests/Features/Category/DeleteCategory/DeleteCategoryCommandTest.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Application.Features.Category.DeleteCategory;
+using Catalog.Application.Repositories;
+using NSubstitute;
+
+namespace Catalog.Application.UnitTests.Features.Category.DeleteCategory
+{
+    public class DeleteCategoryCommandTest
+    {
+        public static readonly DeleteCategoryCommand deleteCategoryCommand = new DeleteCategoryCommand(Guid.NewGuid());
+        private readonly DeleteCategoryCommandHandler _deleteCategoryCommand;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly DeleteCategoryValidator _validator;
+        public DeleteCategoryCommandTest()
+        {
+            _unitOfWork=Substitute.For<IUnitOfWork>();
+            _deleteCategoryCommand = new DeleteCategoryCommandHandler(_unitOfWork);
+            _validator = new DeleteCategoryValidator();
+        }
+    }
+}
